a930575bab47e2b87fd30631333c831deb85835e
Per Christian Moen <per.christian.moen@nav.no>
2020-09-04 16:47:45 +0200

    Remove Person-Feed

--- model/src/main/java/no/nav/foreldrepenger/vtp/testmodell/feed/HendelseContent.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.vtp.testmodell.feed;
-
-public interface HendelseContent {
-
-    String AKTØR_ID_IDENT_TYPE = "AktoerId";
-    String FNR_IDENT_TYPE = "NorskIdent";
-
-    String hentType();
-}
