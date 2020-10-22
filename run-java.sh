c7d0484d3884c7ad86f0e2a534ae440c82598f97
Per Christian Moen <per.christian.moen@nav.no>
2020-10-22 20:48:46 +0200

    Update package

--- run-java.sh
+++ run-java.sh
@@ -6,9 +6,9 @@ if [[ $MSYSTEM =~ MINGW* ]]; then
 		-Dlogback.configurationFile=logback.xml \
 		-Dfile.encoding=UTF8 \
 		-Dscenarios.dir=../model/scenarios \
-		no.nav.foreldrepenger.vtp.server.MockServer) 
+		no.nav.pensjon.vtp.server.MockServer)
 else 
 	exec java ${DEFAULT_JAVA_OPTS} ${JAVA_OPTS} -cp app.jar:lib/* \
 		-Dlogback.configurationFile=logback.xml \
-		-Dfile.encoding=UTF8 no.nav.foreldrepenger.vtp.server.MockServer
+		-Dfile.encoding=UTF8 no.nav.pensjon.vtp.server.MockServer
 fi
