c643a01e588ed32caea574642ec1127cf328e160
Anton Fofanov <anton.fofanov@nav.no>
2020-10-26 17:17:24 +0100

    update MEDL2 Mock

--- mocks/medl-mock/src/main/java/no/nav/pensjon/vtp/mocks/medl/medl2/rest/api/v1/MedlemskapsunntakMock.java
+++ mocks/medl-mock/src/main/java/no/nav/pensjon/vtp/mocks/medl/medl2/rest/api/v1/MedlemskapsunntakMock.java
@@ -1,39 +1,17 @@
 package no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1;
 
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_OPERATION_MEDLEMSKAPSUNNTAK;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_OPERATION_MEDLEMSKAPSUNNTAK_I_PERIODE;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_EKSKLUDER_KILDER;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_FRA_OG_MED;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_INKLUDER_SPORINGSINFO;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_INKLUDER_SPORINGSINFO_PERSON;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_PERSONIDENT;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_STATUSER;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_TIL_OG_MED;
-import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.API_PARAM_UNNTAK_ID;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
-
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-
 import no.nav.pensjon.vtp.core.annotations.JaxrsResource;
+
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.Set;
+
+import static no.nav.pensjon.vtp.mocks.medl.medl2.rest.api.v1.MedlemskapsunntakApiParams.*;
 
 @JaxrsResource
 @Path("medl2/api/v1/medlemskapsunntak")
@@ -54,30 +32,9 @@ public class MedlemskapsunntakMock {
     @Consumes(MediaType.APPLICATION_JSON)
     public List<Object> hentMedlemskapsunntakIPeriode(
             @ApiParam(API_PARAM_STATUSER) @QueryParam("statuser") Set<String> statuser,
-            @ApiParam(API_PARAM_FRA_OG_MED) @QueryParam("fraOgMed") Dato fraOgMed,
-            @ApiParam(API_PARAM_TIL_OG_MED) @QueryParam("tilOgMed") Dato tilOgMed,
-            @ApiParam(API_PARAM_INKLUDER_SPORINGSINFO_PERSON) @QueryParam("inkluderSporingsinfo") boolean sporing,
-            @ApiParam(API_PARAM_EKSKLUDER_KILDER) @QueryParam("ekskluderKilder") Set<String> ekskluder,
-            @ApiParam(value = API_PARAM_PERSONIDENT, required = true) @NotNull @QueryParam("Nav-Personident") String ident) {
+            @ApiParam(API_PARAM_INKLUDER_SPORINGSINFO_PERSON) @QueryParam("inkluderSporingsinfo") boolean sporing) {
 
         return List.of();
-    }
-
-    private static class Dato {
-        private static final ObjectMapper objectMapper = new ObjectMapper();
-        static {
-            objectMapper.registerModule(new Jdk8Module());
-            objectMapper.registerModule(new JavaTimeModule());
-        }
-
-        LocalDate dato; //NOSONAR
-
-        public Dato(String localDate) {
-            try {
-                dato = objectMapper.readValue(localDate, LocalDate.class);
-            } catch (JsonProcessingException ignore) {
-            }
-        }
     }
 }
 
