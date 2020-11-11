385e6ed3f13acd1e7df1a4171424cbc5e92efa2e
Eziz Annagurban <eziztm@yahoo.com>
2020-11-11 13:27:05 +0100

    changed EnonicMock requestMapping path

--- mocks/enonic-mock/src/main/java/no/nav/pensjon/vtp/mocks/enonic/EnonicMock.java
+++ mocks/enonic-mock/src/main/java/no/nav/pensjon/vtp/mocks/enonic/EnonicMock.java
@@ -1,5 +1,7 @@
 package no.nav.pensjon.vtp.mocks.enonic;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +9,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
 @RestController
 @RequestMapping("/rest/enonic")
 public class EnonicMock {
     private static final Logger logger = LoggerFactory.getLogger(EnonicMock.class);
 
-    @GetMapping(value = "/*")
-    public ResponseEntity get() {
-        logger.info("Fikk en forespørsel på /rest/enonic");
+    @GetMapping(value = "/**")
+    public ResponseEntity get(HttpServletRequest request) {
+        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+        logger.info("Fikk en forespørsel på {}", path);
+
         return ResponseEntity.ok().build();
     }
 }
