bb42e1e457e3df958d7bfc70283545f1a31070cb
Per Christian Moen <per.christian.moen@nav.no>
2020-11-23 11:23:27 +0100

    Konvert classes to Kotlin

---
+++ miscellaneous/src/main/java/no/nav/pensjon/vtp/miscellaneous/rest/IsReadyImpl.kt
@@ -0,0 +1,17 @@
+package no.nav.pensjon.vtp.miscellaneous.rest
+
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@Api(tags = ["isReady"])
+@RequestMapping("/rest/isReady")
+class IsReadyImpl {
+    @GetMapping(produces = [MediaType.TEXT_PLAIN_VALUE])
+    @ApiOperation(value = "isReady", notes = "Sjekker om systemet er ready for NAIS")
+    fun buildPermitResponse() = "OK"
+}
