56ebb4af011c847a43502f83110d028cded6a45c
Anton Fofanov <anton.fofanov@nav.no>
2021-02-01 16:10:53 +0100

    remove unnessesary property declarations in data class

--- model/src/main/kotlin/no/nav/pensjon/vtp/testmodell/pensjon_testdata/Handlebar.kt
+++ model/src/main/kotlin/no/nav/pensjon/vtp/testmodell/pensjon_testdata/Handlebar.kt
@@ -1,9 +1,3 @@
 package no.nav.pensjon.vtp.testmodell.pensjon_testdata
 
-import java.util.ArrayList
-
 data class Handlebar(val handlebar: String, val inputtype: String, val validators: List<String>)
-
-val handlebar: String? = null
-val inputtype = "text"
-val validators: List<String> = ArrayList()
