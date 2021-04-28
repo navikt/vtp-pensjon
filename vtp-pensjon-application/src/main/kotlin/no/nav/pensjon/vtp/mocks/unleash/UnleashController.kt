fdb43627cfcf46c26b4d0e8e6e49961b7a81b6cb
Per Christian Moen <per.christian.moen@nav.no>
2021-04-28 19:08:21 +0200

    Add Unleash mock

---
+++ vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/mocks/unleash/UnleashController.kt
@@ -0,0 +1,52 @@
+package no.nav.pensjon.vtp.mocks.unleash
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.lang.System.currentTimeMillis
+import java.time.LocalDate.now
+import java.util.*
+import javax.servlet.http.Cookie
+import javax.servlet.http.HttpServletResponse
+
+@RestController
+@RequestMapping("/rest/unleash")
+class UnleashController {
+    @GetMapping("/api/client/features")
+    fun features() = FeatureResource(
+        features = listOf(Feature(
+            name = "pensjon.tp-ejb-adapter.tp-nais",
+            description = "Controls the use of tjenestepensjon on nais / was",
+            enabled = true
+        ))
+    )
+
+    @PostMapping("/api/client/metrics")
+    fun metrics(response: HttpServletResponse) = with(response) {
+        addCookie(Cookie(
+            "unleash-session",
+            "eyJub3dJbkhvdXJzIjo0NDk4OTZ9"
+        ))
+        addCookie(Cookie(
+            "unleash-session.sig",
+            "GkWsWE-O5XwV5rW11CCodmpkn8"
+        ))
+        status = 202
+    }
+
+    data class FeatureResource(val version: Int = 1, val features: List<Feature>)
+
+    data class Feature(
+        val name: String,
+        val description: String,
+        val enabled: Boolean,
+        val strategies: List<Strategy> = listOf(Strategy("default")),
+        val variants: String? = null,
+        val createdAt: String = now().toString()
+    )
+
+    data class Strategy(
+        val name: String
+    )
+}
