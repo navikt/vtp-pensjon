5c2fef84a713ab3fcfab4909ce42cb05e0ddb8ce
henrikolsvik <henrik-olsvik@outlook.com>
2021-11-18 16:14:20 +0100

    Lint

--- vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/auth/idporten/IdPortenMock.kt
+++ vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/auth/idporten/IdPortenMock.kt
@@ -130,7 +130,6 @@ class IdPortenMock(
         return ResponseEntity.status(200).body(idPortenSession.code)
     }
 
-
     @GetMapping(value = ["/login"])
     fun doLogin(@RequestParam("redirect") redirect: String, @RequestParam("pid") pid: String, @RequestParam("nonce", required = false) nonce: String, @RequestParam("state") state: String): ResponseEntity<Any> {
         val idPortenSession = idPortenSessionRepository.save(IdPortenSession(pid = pid, nonce = nonce))
