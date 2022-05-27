6c05fb489ed1d56b3c7800d6256e09468725c691
gardsabo <gard.sebo@nav.no>
2022-05-27 15:01:14 +0200

    Implementert SOAP kall til hentFamiliehistorikk

--- vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/mocks/psak/PselvPersonServiceMockImpl.kt
+++ vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/mocks/psak/PselvPersonServiceMockImpl.kt
@@ -247,7 +247,8 @@ class PselvPersonServiceMockImpl(private val psakpselvPersonAdapter: PsakpselvPe
     )
     @WebResult(name = "hentFamilierelasjonsHistorikkResponse")
     override fun hentFamilierelasjonsHistorikk(@WebParam(name = "hentFamilierelasjonsHistorikkRequest") var1: ASBOPenHentFamilierelasjonsHistorikkRequest) =
-        throw NotImplementedException()
+        psakpselvPersonAdapter.getASBOPenPerson(var1.fnr)
+            ?: throw HentSamboerforholdFaultPenGeneriskMsg()
 
     @WebMethod
     @RequestWrapper(
