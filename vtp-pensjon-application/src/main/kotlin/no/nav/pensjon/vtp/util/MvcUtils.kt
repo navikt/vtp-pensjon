dd5d1bff1cc6c80f2d0527c095d26aeeb4646229
Per Christian Moen <per.christian.moen@nav.no>
2022-06-02 08:49:09 +0200

    Add scope and audience as parameters to sts tokens

--- vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/util/MvcUtils.kt
+++ vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/util/MvcUtils.kt
@@ -1,11 +1,12 @@
 package no.nav.pensjon.vtp.util
 
+import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.notFound
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
 
-fun <T> T?.asResponseEntity() = this
+fun <T> T?.asResponseEntity(): ResponseEntity<T> = this
     ?.let(::ok)
     ?: notFound().build()
 
