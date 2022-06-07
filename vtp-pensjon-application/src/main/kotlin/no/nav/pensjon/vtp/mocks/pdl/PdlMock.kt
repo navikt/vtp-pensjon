15384fb666f2ac03327149ad643341186f6572e6
a150238 <eivind.askestad@nav.no>
2022-06-07 13:18:07 +0200

    Pdl mock for POPP
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/mocks/pdl/PdlMock.kt
+++ vtp-pensjon-application/src/main/kotlin/no/nav/pensjon/vtp/mocks/pdl/PdlMock.kt
@@ -4,7 +4,11 @@ import graphql.ExecutionInput
 import graphql.GraphQL
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @Tag(name = "PDL")
@@ -19,6 +23,44 @@ class PdlMock(private val graphQL: GraphQL) {
             .build()
             .let { graphQL.execute(it) }
             .toSpecification()
+
+    @PostMapping(path = ["", "forpopp/graphql"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun pdlForPoppRequest(@RequestBody request: PdlRequest): ResponseEntity<String> {
+        val requestedIdent = request.variables["ident"]
+        return ResponseEntity.ok(createPdlResponseForPopp(requestedIdent))
+    }
+
+    private fun createPdlResponseForPopp(ident: Any?): String {
+        return """
+            {
+              "data": {
+                "hentPerson": {
+                  "folkeregisteridentifikator": [
+                    {
+                      "identifikasjonsnummer": "$ident",
+                      "status": "I_BRUK",
+                      "type": "FNR",
+                      "metadata": {
+                        "historisk": false,
+                        "master": "FREG"
+                      }
+                    }
+                  ],
+                  "foedsel": [
+                    {
+                      "foedselsaar": 1988,
+                      "metadata": {
+                        "historisk": false,
+                        "master": "FREG",
+                        "endringer": []
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+        """.trimIndent()
+    }
 }
 
 data class PdlRequest(val query: String, val variables: Map<String, Any>)
